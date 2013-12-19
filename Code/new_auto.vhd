7dfbd23feaff0e71bb5e72861342588e068b438b
Dan Pipe-Mazo <dpipemazo@gmail.com>
2013-12-18 21:11:51 -0800

    Actually fixed 'final_hamming'

--- Code/new_auto.vhd
+++ Code/new_auto.vhd
@@ -437,7 +437,7 @@ begin
 	end generate genham7s;
 
 	-- Put together the final adder, and then we have our value!
-	final_hamming <= std_logic_vector(unsigned(hamming_7s(0)(7 downto 0)) + unsigned(hamming_7s(1)(7 downto 0)));
+	final_hamming <= std_logic_vector(("0" & unsigned(hamming_7s(0))) + ("0" & unsigned(hamming_7s(1))));
 
 	--
 	---
