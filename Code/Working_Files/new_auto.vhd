e5115650f8be250e0aab478526ca715ba7810e9b
Dan Pipe-Mazo <dpipemazo@gmail.com>
2013-12-20 23:14:50 -0800

    Don't really like the logic as is, but synthesis should clean it up. Should work now

--- Code/Working_Files/new_auto.vhd
+++ Code/Working_Files/new_auto.vhd
@@ -403,7 +403,9 @@ begin
 	--	or once we find a maximum autocorrelation value which is not the 
 	--	first index. 
 	cycle_done <= '1' when ( (samp_counter(11) and samp_counter(7)) = '1' ) or
-						   ( (valid_auto = '1') and (not std_match(max_idx_val, std_logic_vector(to_unsigned(1, max_idx_val'length)))) ) else
+						   ( (valid_auto = '1') and 
+						   	 (not std_match(max_idx_val, std_logic_vector(to_unsigned(1, max_idx_val'length)))) and
+						   	 (not std_match(max_idx_val, std_logic_vector(to_unsigned(0, max_idx_val'length)))) ) else
 				  '0';
 
 	-- We are completely done when the old divider is equal to the new divider and 
