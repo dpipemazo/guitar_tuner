f7aff3d400ed81a8b26635e1b47185b38e6163c3
Dan Pipe-Mazo <dpipemazo@gmail.com>
2013-12-20 23:04:44 -0800

    fixed mismatching parens

--- Code/Working_Files/new_auto.vhd
+++ Code/Working_Files/new_auto.vhd
@@ -402,8 +402,8 @@ begin
 	-- We are done with a cycle when the cycle counter has reached its maximum
 	--	or once we find a maximum autocorrelation value which is not the 
 	--	first index. 
-	cycle_done <= '1' when ((samp_counter(11) and samp_counter(7)) = '1') or
-						   ((valid_auto = '1') and (not std_match(max_idx_val, std_logic_vector(to_unsigned(1, max_idx_val'length)))) else
+	cycle_done <= '1' when ( (samp_counter(11) and samp_counter(7)) = '1' ) or
+						   ( (valid_auto = '1') and (not std_match(max_idx_val, std_logic_vector(to_unsigned(1, max_idx_val'length)))) ) else
 				  '0';
 
 	-- We are completely done when the old divider is equal to the new divider and 
