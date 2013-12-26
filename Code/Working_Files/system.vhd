f62091dbc5f41c56c593b593d83401b135352c73
Dan Pipe-Mazo <dpipemazo@gmail.com>
2013-12-25 20:37:10 -0800

    changing thresholding to simple divide by 2 to see if I can get better results

--- Code/Working_Files/system.vhd
+++ Code/Working_Files/system.vhd
@@ -199,7 +199,7 @@ begin
     --  to make sure that we aren't spitting out
     --  samples faster than it can handle
     --
-    sample_done_sig <= (auto_done and do_sample and disp_fifo_empty);
+    sample_done_sig <= (auto_done and do_sample and disp_fifo_empty and sample_valid);
 
     --
     -- Put interesting things on the LEDs
