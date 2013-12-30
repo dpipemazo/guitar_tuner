8a2741e7a9d569cc0d5d9552bb33ca123237616e
Dan Pipe-Mazo <dpipemazo@gmail.com>
2013-12-29 22:06:29 -0800

    Changing autocorrelation to only look for frequency of interest when tuning a string, else run as usual. Should hopefully fix issues being seen

--- Code/Working_Files/freq_constants.vhd
+++ Code/Working_Files/freq_constants.vhd
@@ -14,12 +14,12 @@ package freq_constants is
 	type q_14_10_freqs 	is array(0 to 5) of std_logic_vector(23 downto 0);
 
 	-- Define the frequencies
-	constant e2_082_407_hz 	: std_logic_vector(23 downto 0) := "000000010100100110100000";
-	constant a2_110_000_hz	: std_logic_vector(23 downto 0)	:= "000000011011100000000000";
-	constant d3_146_832_hz	: std_logic_vector(23 downto 0)	:= "000000100100101101010011";
-	constant g3_195_998_hz	: std_logic_vector(23 downto 0)	:= "000000110000111111111101";
-	constant b3_246_942_hz	: std_logic_vector(23 downto 0)	:= "000000111101101111000100";
-	constant e4_329_628_hz	: std_logic_vector(23 downto 0)	:= "000001010010011010000011";
+	constant e2_082_407_hz 	: std_logic_vector(23 downto 0) := "000000010100100110100000";	-- 82.407 
+	constant a2_110_000_hz	: std_logic_vector(23 downto 0)	:= "000000011011100000000000";	-- 110.000
+	constant d3_146_832_hz	: std_logic_vector(23 downto 0)	:= "000000100100101101010011";	-- 146.832
+	constant g3_195_998_hz	: std_logic_vector(23 downto 0)	:= "000000110000111111111101";	-- 195.998
+	constant b3_246_942_hz	: std_logic_vector(23 downto 0)	:= "000000111101101111000100";	-- 246.942
+	constant e4_329_628_hz	: std_logic_vector(23 downto 0)	:= "000001010010011010000011";	-- 329.628
 
 	-- Define the array of frequencies
 	constant expected_freqs : q_14_10_freqs :=	(
@@ -30,6 +30,28 @@ package freq_constants is
 													b3_246_942_hz,
 													e4_329_628_hz
 												);
+
+	-- Define the array of sampling dividers to use
+	type string_divider is array(0 to 5) of std_logic_vector(11 downto 0);
+
+	-- Define the dividers
+	constant e2_divider : std_logic_vector(23 downto 0) := "010010100001"; -- 1185
+	constant a2_divider : std_logic_vector(23 downto 0) := "001101111000"; -- 888
+	constant d3_divider : std_logic_vector(23 downto 0) := "001010011001"; -- 665
+	constant g3_divider : std_logic_vector(23 downto 0) := "000111110010"; -- 498
+	constant b3_divider : std_logic_vector(23 downto 0) := "000110001100"; -- 396
+	constant e4_divider : std_logic_vector(23 downto 0) := "000100101000"; -- 296
+
+	-- Define the array of dividers
+	constant string_dividers : string_divider := 	(
+														e2_divider,
+														a2_divider,
+														d3_divider,
+														g3_divider,
+														b3_divider,
+														e4_divider
+													);
+
 
 
 end package;
